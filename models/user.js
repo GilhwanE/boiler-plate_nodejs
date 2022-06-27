9bb32320b8b2e6728fbc5072ce380a52eb6e1e70
gilhwan <dyghks123@gmail.com>
2022-06-27 14:25:35 +0900

    ✨ Func user password hash

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -30,6 +33,26 @@ const userSchema = mongoose.Schema({
   },
 });
 
+userSchema.pre('save', function (next) {
+  var user = this;
+  // 비밀번호를 암호화 시킨다.
+
+  if (user.isModified('password')) {
+    // 비밀번호 수정될때만 실행
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash; // plaintextPassword를 hash로 변경하는 과정
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+}); // 몽구스의 메서드
+
 const User = mongoose.model('User', userSchema);
 
-module.exports = { User };
+module.exports = { User }; // 다른곳에서도 사용할 수 있도록
